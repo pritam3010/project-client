8dddb473b048813f0eed59b0b9b75c17c005f6be
Pritam Kumar <pk.rajece2010@gmail.com>
2019-10-18 21:57:15 +0530

    Change api URL for the deployment on aws

---
+++ build/precache-manifest.420a4fdae45e5120fa6f38536e1d9e27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012c891f15c9b1a0e07b794f2011fb7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c50274a84bc5a9868bd",
+    "url": "/static/css/2.239f4597.chunk.css"
+  },
+  {
+    "revision": "5c50274a84bc5a9868bd",
+    "url": "/static/js/2.25b7e896.chunk.js"
+  },
+  {
+    "revision": "715f5745c06ed7ac299f",
+    "url": "/static/js/main.be6fc77a.chunk.js"
+  },
+  {
+    "revision": "2bff3d60d30ac8d79da4",
+    "url": "/static/js/runtime~main.f03489af.js"
+  },
+  {
+    "revision": "ad598f6b2703fcdac4c3dbd50e0fb2b6",
+    "url": "/static/media/mountain-blurred.ad598f6b.jpg"
+  },
+  {
+    "revision": "07b951d839b6852b9e2e3b08dc6adf8b",
+    "url": "/static/media/mountain.07b951d8.jpg"
+  }
+]);
